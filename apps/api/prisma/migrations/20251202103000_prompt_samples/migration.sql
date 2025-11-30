cef5c5218426373ca8d3d55b61e79f4d56d07b65
李碧强 <624144061@qq.com>
2025-11-30 22:04:40 +0800

    commit

--- apps/api/prisma/migrations/20251202103000_prompt_samples/migration.sql
+++ apps/api/prisma/migrations/20251202103000_prompt_samples/migration.sql
@@ -17,7 +17,7 @@ CREATE TABLE "PromptSample" (
 );
 
 -- AddForeignKey
-ALTER TABLE "PromptSample" ADD CONSTRAINT "PromptSample_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "PromptSample" ADD CONSTRAINT "PromptSample_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- Indexes
 CREATE INDEX "PromptSample_userId_idx" ON "PromptSample"("userId");
